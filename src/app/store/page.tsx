ce6266f71b170cfb13683e389369ff641cb8be97
KrisOP <krisop@dat-analytics.com>
2025-03-24 17:12:30 -0600

    cambiando titulo en store

--- src/app/store/page.tsx
+++ src/app/store/page.tsx
@@ -1,7 +1,7 @@
 export default function Store() {
   return (
     <div>
-      <h1 className="font-bold text-2xl mt-4">Este es el STORE</h1>
+      <h1 className="font-bold text-2xl mt-4">Este es el STORE KOP</h1>
     </div>
   );
 }
