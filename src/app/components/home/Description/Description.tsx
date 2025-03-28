a98d7c989213f38319f655892650857a87ac36ad
KrisOP <krisop@dat-analytics.com>
2025-03-28 14:06:43 -0600

    hasta clase 13, clases globales y scss fallido

--- src/app/components/home/Description/Description.tsx
+++ src/app/components/home/Description/Description.tsx
@@ -2,7 +2,9 @@ export const Description = ()=>{
     return(
         <section>
             <h1>Description corriendo</h1>
+            <img src="/images/back.jpg" alt="description" />
+            <h2> Description</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis esse tenetur et ad illo inventore vero molestiae hic voluptatibus distinctio perspiciatis, sunt eius dicta maxime sit perferendis dolorum sed voluptates?</p>
         </section>
-       
     )
 }
